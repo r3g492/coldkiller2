a1ffd90c71f6cb57654929ab27475087acd4a5aa
r3g492 <kangggunw3492@gmail.com>
2025-12-25 14:07:50 +0900

    damage 50 -> 30

--- bullet/manager.go
+++ bullet/manager.go
@@ -59,7 +59,7 @@ func (bm *Manager) Mutate(dt float32, p *killer.Killer, el []enemy.Enemy) {
 			enemySize := el[j].Size
 			curBullet := bm.Bullets[i]
 			if rl.Vector3Distance(enemyPos, curBullet.Position) < enemySize && el[j].Health > 0 {
-				el[j].Damage(50)
+				el[j].Damage(30)
 				bm.Bullets[i].Active = false
 			}
 		}
